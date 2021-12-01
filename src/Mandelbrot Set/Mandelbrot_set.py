7099033823ceb736892eeec8d7d759931d9d0d86
ReiYamahara202105260 <rei.yamahara@gmail.com>
2021-12-01 12:52:39 +0000

    julia and mandelbrot set

--- src/Mandelbrot Set/Mandelbrot_set.py
+++ src/Mandelbrot Set/Mandelbrot_set.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-y_pixels = 1000
+y_pixels = 300
 x_pixels = round((2/3) * y_pixels)
 array=np.zeros((x_pixels, y_pixels))
 x=np.linspace(-2, 1, y_pixels)
 y=np.linspace(-1.2, 1.2, x_pixels)
 
 def mandelbrot_set(Re,Im, max_iter):
-    c=complex(Re,Im)
-    z=0
+    z=complex(Re,Im)
+    c = 0
     for i in range(max_iter):
         z=z**2+c
         if abs(z)>=2:
@@ -18,10 +18,10 @@ def mandelbrot_set(Re,Im, max_iter):
 
 for b in y:
    for a in x:
-        array[np.where(y==b),np.where(x==a)]=mandelbrot_set(a,b,30)
+        array[np.where(y==b),np.where(x==a)]=mandelbrot_set(a,b,10)
         
      
-fig=plt.figure(dpi=200)
+fig=plt.figure(dpi=100)
 plt.imshow(array,cmap='Reds',extent=[-2,2,-2,2],interpolation='None')
 plt.axis("off")
 plt.show();
--- src/Mandelbrot Set/mandelbrot_set_final.py
+++ src/Mandelbrot Set/mandelbrot_set_final.py
@@ -8,11 +8,13 @@ y=np.linspace(-1.2, 1.2, y_pixels) * 1j
 complex_num = x + y.reshape(y_pixels, 1)
 array=np.zeros((y_pixels, x_pixels))
 
-iteration = 4
+iteration = 30
 z = 0
 
-for i in range(1, iteration + 1): # from 1 to iteration + 1 because array is a zero array
+
+for i in range(0, iteration + 1): # from 1 to iteration + 1 because array is a zero array
     z = z ** 2 + complex_num
+
     condition = (abs(z) >= 2) & (array == 0) # absolute value of z is greater than 0 and the array value has not been filled
     array[condition] = i
 else:
@@ -21,4 +23,9 @@ else:
 plt.figure(dpi = 100)
 plt.imshow(array, cmap = 'RdBu', interpolation = 'None')
 plt.axis("off")
-plt.show();
+plt.show()
+print(array);
+
+# possible improvements:
+# - what to do if z becomes very large
+# what to do if the z becomes really small
