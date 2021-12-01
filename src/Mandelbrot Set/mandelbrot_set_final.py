282020505bbee43e3f321b0104c4e0962aa8fafa
ReiYamahara202105260 <rei.yamahara@gmail.com>
2021-12-01 11:45:51 +0000

    mandelbrot_animation

--- src/Mandelbrot Set/mandelbrot_set_final.py
+++ src/Mandelbrot Set/mandelbrot_set_final.py
@@ -1,24 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x_pixels = 10000
+x_pixels = 1000
 y_pixels = round((2/3) * x_pixels)
 x=np.linspace(-2.1, 1.2, x_pixels) # get 3:2 ratio because it looks good 
 y=np.linspace(-1.2, 1.2, y_pixels) * 1j
 complex_num = x + y.reshape(y_pixels, 1)
 array=np.zeros((y_pixels, x_pixels))
 
-iteration = 50
+iteration = 4
 z = 0
+
 for i in range(1, iteration + 1): # from 1 to iteration + 1 because array is a zero array
     z = z ** 2 + complex_num
     condition = (abs(z) >= 2) & (array == 0) # absolute value of z is greater than 0 and the array value has not been filled
     array[condition] = i
-else: 
+else:
     array[array == 0] = iteration
-array = array - 1
 
-fig = plt.figure(dpi = 1000)
+plt.figure(dpi = 100)
 plt.imshow(array, cmap = 'RdBu', interpolation = 'None')
 plt.axis("off")
 plt.show();
---
+++ src/Mandelbrot set animation/mandelbrot_set_animation_1.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x_pixels = 1000
+y_pixels = round((2/3) * x_pixels)
+x=np.linspace(-2.1, 1.2, x_pixels) # get 3:2 ratio because it looks good 
+y=np.linspace(-1.2, 1.2, y_pixels) * 1j
+complex_num = x + y.reshape(y_pixels, 1)
+array=np.zeros((y_pixels, x_pixels))
+
+iteration = 10
+z = 0
+
+for i in range(1, iteration + 1): # from 1 to iteration + 1 because array is a zero array    
+    plt.clf() # clears the figure
+    plt.figure(1, dpi = 100) # only on unique figure 1 - we don't create numerous figures
+    
+    array[array == iteration] = 0 # reverts the inner colour back to 0 (nothing happens first time round)
+    z = z ** 2 + complex_num
+    condition = (abs(z) >= 2) & (array == 0) # absolute value of z is greater than 0 and the array value has not been filled
+    array[condition] = i
+    array[array == 0] = iteration # the inner colour
+    plt.imshow(array, cmap = 'RdBu', interpolation = 'None')
+    plt.axis("off")
+    plt.pause(0.3) # How long to pause
+plt.show(); 
+
+
