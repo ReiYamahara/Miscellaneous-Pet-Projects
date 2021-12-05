735e6ff0cea3f73bda7633070a615e042e9fec2e
ReiYamahara202105260 <rei.yamahara@gmail.com>
2021-12-05 20:15:03 +0000

    in progress

--- src/Apollonian Gasket/README.md
+++ src/Apollonian Gasket/README.md
@@ -1,3 +1,17 @@
 Apollonian Basket:
 
 The maths behind the Apollonian Basket:
+
+The curvature/bend of a circle is defined to be the reciprocal of its radius.
+- Negative curvature indicates that all other circles are internally tangent to that circle (inside the circle so it only applies to the first outer circle, in which we will plot all the other circles)
+- A zero curvature gives a line (circle with infinite radius)
+- Positive curvature indicates that all other circles are externally tangent to that circle. This circle is in the interior of circle with negative curvature.
+
+
+Parameters:
+- theta: angle
+- starting radius
+
+Initial conditions:
+- the outer circle has radius -1.
+- the apollonian gasket requires three predetermined circles.
--- src/Apollonian Gasket/apollonian_gasket_1.py
+++ src/Apollonian Gasket/apollonian_gasket_1.py
@@ -1,86 +1,101 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import cmath
+import matplotlib.pyplot as plt
+import pandas as pd
+from cmath import *
 
-fig=plt.figure(dpi=100)
+fig, ax = plt.subplots(dpi = 100)
 plt.axis('equal')
-plt.axis('off')
 
-#simplifications
-sin=np.sin
-cos=np.cos
-sqrt=np.sqrt
-
-#setting parameters
+# parameter: angle and radius
 theta = np.linspace(0, 2*np.pi, 100)
-r=1
+r_1 = 1
 
-#Initial conditions:
-a = r*cos(theta) + 0
-b = r*sin(theta)
+# Creating pandas dataframe
+df = pd.DataFrame({'Radius':[-1], 'Centre':[[0, 0]], 
+                'radius_1':[0], 'centre_1':[0], 
+                'radius_2':[0], 'centre_2':[0],
+                'radius_3':[0], 'centre_3':[0]})
 
-c = r/2*cos(theta) - 0.5
-d = r/2*sin(theta)
+df.loc[len(df)] = [1/2, [-1/2, 0], 0, 0, 0, 0, 0, 0]
+df.loc[len(df)] = [1/2, [1/2, 0], 0, 0, 0, 0, 0, 0]
 
-e = r/2*cos(theta) + 0.5
-f = r/2*sin(theta)
+## Initial Conditions:
+# Outer circle:
+x_1 = r_1*np.cos(theta)
+y_1 = r_1*np.sin(theta)
+ax.plot(x_1, y_1)
 
-plt.plot(a,b,linewidth=1)
-plt.plot(c,d,linewidth=1)
-plt.plot(e,f,linewidth=1)
+# Second circle:
+r_2 = r_1 / 2
+x_2 = r_2 * np.cos(theta) - 0.5
+y_2 = r_2 * np.sin(theta)
+ax.plot(x_2, y_2)
+
+# Third circle:
+r_3 = r_1 / 2
+x_3 = r_3 * np.cos(theta) + 0.5
+y_3 = r_3 * np.sin(theta)
+ax.plot(x_3, y_3)
+
+# maybe make a pandas dataframe for each circle: for each new circle, there'll be the radius and the centre of the three other circles
+# a tree is more optimal but this will have to do - we will put another parameter - the size of the radius.
+
+# Each circle will have a centre, a radius and a curvature and a 
+
+def radius_and_centre(r_1, centre_1, r_2, centre_2, r_3, centre_3): # it should take in the dataframe... and also a limit parameter (curvature value)
+    '''
+    Parameters:
+    r_1, r_2, r_3: float
+    centre_1, centre_2, centre_3: list of 2 floats
+
+    returns the radius and centres of the 4th circle
+    ''' # what if there are multiple solutions...?
+
+    # curvature of the three cirles
+    curve_1 = 1/r_1
+    curve_2 = 1/r_2
+    curve_3 = 1/r_3
+
+    # radius of the 4th circle # we could get two different radii
+    curve_4 = curve_1 + curve_2 + curve_3 + 2*sqrt(curve_1*curve_2 + curve_2*curve_3 + curve_3*curve_1)
+    r_4 = (1/curve_4).real # converting from complex to real
+
+    # complex number form of the three circles
+    complex_1 = centre_1[0] + centre_1[1]* 1j
+    complex_2 = centre_2[0] + centre_2[1]* 1j
+    complex_3 = centre_3[0] + centre_3[1]* 1j
+
+    # complex number multiplied by curvature
+    compcurve_1 = curve_1 * complex_1 
+    compcurve_2 = curve_2 * complex_2
+    compcurve_3 = curve_3 * complex_3
+
+    # centres of the 4th circle
+    # maybe we need to verify if there is 1 or 2 solutions...?
+    first_centre = (compcurve_1 + compcurve_2 + compcurve_3 + 2*sqrt(compcurve_1*compcurve_2 + compcurve_2*compcurve_3 + compcurve_3*compcurve_1))/curve_4 
+    first_centre_list = [first_centre.real, first_centre.imag]
+
+    second_centre = (compcurve_1 + compcurve_2 + compcurve_3 - 2*sqrt(compcurve_1*compcurve_2 + compcurve_2*compcurve_3 + compcurve_3*compcurve_1))/curve_4
+    second_centre_list = [second_centre.real, second_centre.imag]
+
+    # we need to keep a record of the three circles for each new circle plotted...
+    # not happy that the main dataframe is coming from a global variable... we can fix this later
+    df.loc[len(df)] = [r_4, first_centre_list, r_1, centre_1, r_2, centre_2, r_3, centre_3]
+    df.loc[len(df)] = [r_4, second_centre_list, r_1, centre_1, r_2, centre_2, r_3, centre_3]
+
+def plotter(df):
+    for i in range(2, len(df)):
+        radius = df.iloc[i, 0]
+        centre = df.iloc[i, 1]
+        x = radius*np.cos(theta) - centre[0]
+        y = radius*np.sin(theta) - centre[1]
+        ax.plot(x, y)
+
+radius_and_centre(-1, [0,0], 1/2, [-1/2, 0], 1/2, [1/2, 0])
+plotter(df)
+
+plt.show()
+print(df);
 
 
-#defining each formula that computes the new radius and the new centre
-# x y z are the radius
-# a b c are the centres
-def g(x,y,z):
-    C1=1/x
-    C2=1/y
-    C3=1/z
-    C4=C1+C2+C3+2*sqrt(C1*C2+C2*C3+C3*C1)
-    new_rad = 1/C4
-    return new_rad
-
-def h(x,y,z,a,b,c):
-    C1=1/x
-    C2=1/y
-    C3=1/z
-    C4=C1+C2+C3+2*sqrt(C1*C2+C2*C3+C3*C1)
-    D1 = C1*a
-    D2 = C2*b
-    D3 = C3*c
-    D4= D1+D2+D3+2*cmath.sqrt(D1*D2+D2*D3+D3*D1)
-    new_centre=D4/C4
-    return new_centre
-
-radius=np.array([-1,1/2,1/2])
-centres=np.array([0,1/2,-1/2])
-#try without using arrays...
-for i in range(10):
-    radius=np.append(radius,g(-1,1/2,radius[2+i]))
-    centres=np.append(centres, h(-1,1/2,radius[2+i],0,1/2,centres[2+i]))
-    a = radius[2+i]*cos(theta)+centres[2+i].real
-    b = radius[2+i]*sin(theta)+centres[2+i].imag
-    plt.plot(a,b,linewidth=1)
-
-    
-#Trouble problem here...   
-for i in range(3):
-    radius[5+i]=g(-1,1/3,radius[4+i])
-    centres[5+i]=h(-1,1/3,radius[4+i],0,2/3j,centres[4+i])
-    
-    c = radius[5+i]*cos(theta)+centres[5+i].real
-    d = radius[5+i]*sin(theta)+centres[5+i].imag
-    plt.plot(c,d,linewidth=1)
-
-
-print(radius)
-print(centres)
-plt.scatter(0.5,2/3)
-plt.scatter(0.6428571428571428,0)
-
-print(radius[4],centres[4])
-print(h(-1,1/3,radius[4],0,2/3j,centres[4]))   
-print(h(-1,1/3,1/6,0,2/3j,1/2+2/3j))
-plt.show();
-
+# we need to create a tree of some sorts...?
